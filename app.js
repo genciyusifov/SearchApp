235a8373fd7f3f660eb7c7a861b0b8970ad6f343
Gənci <yusifovgenci91@gmail.com>
2023-05-18 17:36:21 +0400

    Loading Spinner Add

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const searchInput = document.getElementById("inputS");
 const clearBtn = document.getElementById("clear");
 const picture = document.querySelector(".pictureDiv");
-const h1 = document.querySelector("h1")
+const h1 = document.querySelector("h1");
 let value;
 
-function getir() {
+function showLoadingSpinner() {
+    picture.innerHTML = '<div class="loading-spinner"></div>';
+}
+
+function hideLoadingSpinner() {
+    const loadingSpinner = document.querySelector(".loading-spinner");
+    if (loadingSpinner) {
+        loadingSpinner.remove();
+    }
+}
+
+function fetchImages() {
     value = searchInput.value;
+    showLoadingSpinner();
+
     fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
         method: "GET",
         headers: {
@@ -14,38 +27,47 @@ function getir() {
     })
         .then((resp) => resp.json())
         .then(function (data) {
-            if (data.results.length === 0 ) {
-                picture.innerHTML = `<h1>Axtardığınız nədisə onu tapa bilmədim</h1>
-                                            <button id="ozu" onclick="okay()" class="okey">Başa düşdüm</button>
+            if (data.results.length === 0) {
+                picture.innerHTML = `
+                    <h1>Axtardığınız nədisə onu tapa bilmədim</h1>
+                    <button id="ozu" onclick="okay()" class="okey">Başa düşdüm</button>
                 `;
             } else {
-                picture.innerHTML = data.results.reduce((kod, item) => (
-                    kod += `<div class="imgDiv"><img src="${item.urls.small}"/></div>` ), "");
+                picture.innerHTML = data.results.reduce(
+                    (kod, item) =>
+                        (kod += `<div class="imgDiv"><img src="${item.urls.small}"/></div>`),
+                    ""
+                );
             }
         })
         .catch(function (error) {
             console.log(error);
-        });
+        })
+        .finally(hideLoadingSpinner);
 }
+
 function clear() {
     searchInput.value = "";
     const imgDivs = document.querySelectorAll(".imgDiv");
     imgDivs.forEach((imgDiv) => imgDiv.remove());
 }
 
-
 function okay() {
-    const ozu = document.getElementById("ozu")
-    const h1 = document.querySelector("h1")
-    h1.style.display = "none"
-    ozu.style.display = "none"
+    const ozu = document.getElementById("ozu");
+    h1.style.display = "none";
+    ozu.style.display = "none";
 }
 
-// Enter ile axtaris
-searchInput.addEventListener("keyup", function (event) {
+function handleKeyPress(event) {
     if (event.key === "Enter") {
         getir();
     }
-});
+}
 
+// Enter ile axtaris
+searchInput.addEventListener("keyup", handleKeyPress);
 clearBtn.addEventListener("click", clear);
+
+function getir() {
+    fetchImages();
+}
