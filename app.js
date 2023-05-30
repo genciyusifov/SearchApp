4e120549e8009903a4c371d93d418ab2bf08172e
Gənci <yusifovgenci91@gmail.com>
2023-05-30 17:24:02 +0400

    Update

--- app.js
+++ app.js
@@ -5,63 +5,68 @@ const h1 = document.querySelector("h1");
 let value;
 
 function showLoadingSpinner() {
-    picture.innerHTML = '<div class="loading-spinner"></div>';
+  picture.innerHTML = '<div class="loading-spinner"></div>';
 }
 
 function hideLoadingSpinner() {
-    const loadingSpinner = document.querySelector(".loading-spinner");
-    if (loadingSpinner) {
-        loadingSpinner.remove();
-    }
+  const loadingSpinner = document.querySelector(".loading-spinner");
+  if (loadingSpinner) {
+    loadingSpinner.remove();
+  }
 }
 
 function fetchImages() {
-    value = searchInput.value;
-    showLoadingSpinner();
+  value = searchInput.value;
+  showLoadingSpinner();
 
-    fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
-        method: "GET",
-        headers: {
-            Authorization: "Client-ID EpZvern3LOlXOUiYv1BQ4hoXqZ2Zf3spolzF8O9Mgvg",
-        },
+  fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
+    method: "GET",
+    headers: {
+      Authorization: "Client-ID EpZvern3LOlXOUiYv1BQ4hoXqZ2Zf3spolzF8O9Mgvg",
+    },
+  })
+    .then((resp) => resp.json())
+    .then(function (data) {
+      if (data.results.length === 0) {
+        picture.innerHTML = `
+            <h1 id='none'>
+                Axtardığınız " ${value} " bizim şəkil bazamızda tapılmadı :( , Ən qısa zamanda problemi aradan qaldıracayıq
+            </h1>
+            <button id="ozu" onclick="okay()" class="okey">
+                 Başa düşdüm
+            </button>`;
+      } else {
+        picture.innerHTML = data.results.reduce(
+          (kod, item) =>
+            (kod += `<div class="imgDiv"><img src="${item.urls.small}"/></div>`),
+          ""
+        );
+      }
     })
-        .then((resp) => resp.json())
-        .then(function (data) {
-            if (data.results.length === 0) {
-                picture.innerHTML = `
-                    <h1>Axtardığınız nədisə onu tapa bilmədim</h1>
-                    <button id="ozu" onclick="okay()" class="okey">Başa düşdüm</button>
-                `;
-            } else {
-                picture.innerHTML = data.results.reduce(
-                    (kod, item) =>
-                        (kod += `<div class="imgDiv"><img src="${item.urls.small}"/></div>`),
-                    ""
-                );
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .finally(hideLoadingSpinner);
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(hideLoadingSpinner);
 }
 
 function clear() {
-    searchInput.value = "";
-    const imgDivs = document.querySelectorAll(".imgDiv");
-    imgDivs.forEach((imgDiv) => imgDiv.remove());
+  searchInput.value = "";
+  const imgDivs = document.querySelectorAll(".imgDiv");
+  imgDivs.forEach((imgDiv) => imgDiv.remove());
 }
 
 function okay() {
-    const ozu = document.getElementById("ozu");
-    h1.style.display = "none";
-    ozu.style.display = "none";
+  console.log("DELET");
+  const ozu = document.getElementById("ozu");
+  const none = document.getElementById("none");
+  none.style.display = "none";
+  ozu.style.display = "none";
 }
 
 function handleKeyPress(event) {
-    if (event.key === "Enter") {
-        getir();
-    }
+  if (event.key === "Enter") {
+    getir();
+  }
 }
 
 // Enter ile axtaris
@@ -69,5 +74,5 @@ searchInput.addEventListener("keyup", handleKeyPress);
 clearBtn.addEventListener("click", clear);
 
 function getir() {
-    fetchImages();
+  fetchImages();
 }
